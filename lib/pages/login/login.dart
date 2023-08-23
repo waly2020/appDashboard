bd4f42c913108dddc004347521c6f9567f6755e0
waly2020 <walyguema@gmail.com>
2023-08-23 09:41:23 +0100

    firts commit

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -129,7 +129,7 @@ class _LoginPageState extends State<LoginPage> {
                                      Navigator.push(context, PageRouteBuilder(pageBuilder: (_,__,___) => const ContentPages()));
                                   }
                               })();
-                            }
+                            } 
                             FocusScope.of(context).requestFocus(FocusNode());
                           },
                       child: const Text("Se connecter")
